f3987f3fdb5ee1ae669f6ed96d2108aa715869b7
Leonardo Castillo Díaz <leonardocastillod7@gmail.com>
2025-11-19 10:35:09 -0500

    init

--- Codigo/Arduino_ZUrbana/Arduino_ZUrbana.ino
+++ Codigo/Arduino_ZUrbana/Arduino_ZUrbana.ino
@@ -1,6 +1,5 @@
 
 #include <Buzzer.h>
-#include <LiquidCrystal.h>
 #include <DHT.h>
 
 
@@ -10,20 +9,14 @@
 #define DHTTYPE DHT11   // Tipo de sensor DHT
 #define pinSensor A0 // Pin conectado al sensor MQ-135
 
-int vee = 7; 
-int rs = 6;
-int e = 5;
-int d4 = 4;
-int d5 = 3;
-int d6 = 2;
-int d7 = 1;
+
 int temperatura;
 int humedad;
 int buzz = 8;
 
 
 DHT dth(DHTPIN, DHTTYPE);
-LiquidCrystal lcd(rs,e,d4,d5,d6,d7); 
+ 
 ;
 
 // Buzzer buzzer(buzz);
